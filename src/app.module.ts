36e123b1c31988bc1ca874e71727627d07454e10
Pravin <pravin.lolage@b2x.com>
2019-08-06 15:11:18 +0530

    created order controller and create order endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderController } from './order/order.controller';
+import { OrderService } from './order/order.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, OrderController],
+  providers: [AppService, OrderService],
 })
 export class AppModule {}
---
+++ src/order/order.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, HttpCode, BadRequestException } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+@Controller('order')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) { }
+
+  @Post()
+  @HttpCode(200)
+  createOrder(@Body() body: any): object {
+    if (!body) throw new BadRequestException('Please provide valid request body')
+    if (!body.name) throw new BadRequestException('Please provide valid customer name')
+    if (!body.email) throw new BadRequestException('Please provide valid customer email')
+    if (!body.address) throw new BadRequestException('Please provide valid shippig address')
+    if (!body.totalAmount) throw new BadRequestException('Please provide valid order total amount')
+    if (!body.products && !body.products.length) throw new BadRequestException('Please provide valid products')
+
+    return { status: 'working' };
+  }
+}
