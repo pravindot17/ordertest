79a074eb9ca479fb52bdae1a6605bb128fc18382
Pravin Lolage <pravindot17@gmail.com>
2019-08-07 21:58:37 +0530

    removed unnecessary files

--- src/order/create-order.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString, Length, IsNumber } from 'class-validator';
-
-export class CreateOrderDto {
-  public readonly id: string;
-  @IsString() @Length(3, 64) public readonly name: string;
-  @IsEmail() public readonly email: string;
-  @IsString() @Length(3, 64) public readonly address: string;
-  @IsNumber() @Length(1, 5) public readonly totalAmount: number;
-}
