52891d06bad9a220a7695eac442eab621f8c568e
Pravin Lolage <pravindot17@gmail.com>
2019-08-12 17:09:25 +0530

    modified deliver status to 10s

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const PAYMENT_API = 'http://localhost:3002/payment';
 export const AUTH_TOKEN = 'MY_TOP_SECRET_AUTH_STRING';
-export const TIME_TO_DELIVER_INSEC = 5000;
+export const TIME_TO_DELIVER_INSEC = 10000;
 export const USER_PAYMENT_CREDS = {
   method: 'Credit Card',
   cardNumber: '12345667890123123',
